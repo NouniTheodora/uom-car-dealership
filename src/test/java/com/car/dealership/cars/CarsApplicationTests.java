d3f08d11ac5620ebcd9705074e3cddd090ddc9f2
Theodora Nouni <nounitheodora@gmail.com>
2024-10-26 01:30:53 +0300

    Init commit

---
+++ src/test/java/com/car/dealership/cars/CarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.dealership.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
